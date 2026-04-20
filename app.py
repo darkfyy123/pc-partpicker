3175bb4317b28d4206fbc4ee1f3a1f5f1f92fa08
Vít Kotinský <vitkotinsky@gmail.com>
2026-04-20 23:15:24 +0200

    Add SECRET_KEY configuration and modify run method
    Added configuration for SECRET_KEY and updated app.run()

--- app.py
+++ app.py
@@ -8,17 +8,19 @@ from models import GPU, CPU, RAM, MB, CASE, DRIVE, PSU, FAN, BUILD
 
 app = Flask(__name__)
 
-
+# Essential for PythonAnywhere to find your DB
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, "pcparts.db")
-
 
 engine = create_engine(f"sqlite:///{db_path}")
 Session = sessionmaker(bind=engine)
 
 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key')
+
 app.register_blueprint(template)
 app.register_blueprint(function)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
